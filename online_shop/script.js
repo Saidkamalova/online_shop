9055d5a7a049e13a6a8249bd37903eeb7d1f9638
tami131107@gmail.com <akromkhoja.s@gmail.com>
2023-09-29 18:05:00 +0500

    добавили нажатие на Добавить в корзину

--- online_shop/script.js
+++ online_shop/script.js
@@ -1,1 +1,12 @@
-u�Z
+let cart_btn = document.querySelectorAll('.cart-button')
+let total = 0
+for (let i=0; i < cart_btn.length; i += 1){
+    cart_btn[i].addEventListener('click', function(){
+        let amount = document.querySelector('.cart-amount')
+        amount.innerHTML = +amount.innerHTML + 1
+        total += item_prices[i]
+        if (cart.innerHTML != 'Корзина'){
+            cart.innerHTML = total
+        }
+    })
+}
